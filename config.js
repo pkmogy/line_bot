869756e466d1040e5604fcf0cae61d2479a1863e
leelo <powerfish0813@gmail.com>
2018-11-10 17:18:24 +0800

    lineBot to Heroku

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    line: {
+        channelSecret: '2fd7dfa1411f0563419ad645f6f5c489',
+        accessToken: 'WkONZdL/AcYpvv1lDN5MDCj0ZxeI94N4ERiQrkp1KlNF7p+elxkqTBMv+RVhbxPeyiBxCZFGizXkr+yqsyCH93gGOrc2BT/ls+wzlZl36IrICbUZ4Po9nn7twqzzujf8CKbw6OIVnRBMRDRZHxDPRgdB04t89/1O/w1cDnyilFU='
+    },
+    messenger: {
+        accessToken: '',
+        appSecret: '',
+        verifyToken: '',
+    },
+    telegram: {
+        accessToken: '',
+    },
+    olami: {
+        appKey: 'b39708e1b5564962b42c4da34ae5a1c4',
+        appSectet: '0ca5179db9004047a903ec40bcc66d0a'
+    },
+    kkbox: {
+        id: '1b83e2482d85ad7f172634c28c9aefe4',
+        secret: '7bcb063ab09e90211c31300277ccc3ec'
+    }
+}
