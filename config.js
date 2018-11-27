98c1cac6bda27436c352ca1972d68782a7e9162f
PoHanLee <powerfish0813@gmail.com>
2018-11-27 17:47:37 +0800

    更新

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     line: {
-        channelSecret: '2fd7dfa1411f0563419ad645f6f5c489',
-        accessToken: 'WkONZdL/AcYpvv1lDN5MDCj0ZxeI94N4ERiQrkp1KlNF7p+elxkqTBMv+RVhbxPeyiBxCZFGizXkr+yqsyCH93gGOrc2BT/ls+wzlZl36IrICbUZ4Po9nn7twqzzujf8CKbw6OIVnRBMRDRZHxDPRgdB04t89/1O/w1cDnyilFU='
+        channelSecret: '',
+        accessToken: ''
     },
     messenger: {
         accessToken: '',
@@ -12,11 +12,11 @@ module.exports = {
         accessToken: '',
     },
     olami: {
-        appKey: 'b39708e1b5564962b42c4da34ae5a1c4',
-        appSectet: '0ca5179db9004047a903ec40bcc66d0a'
+        appKey: '',
+        appSectet: ''
     },
     kkbox: {
-        id: '1b83e2482d85ad7f172634c28c9aefe4',
-        secret: '7bcb063ab09e90211c31300277ccc3ec'
+        id: '',
+        secret: ''
     }
 }
